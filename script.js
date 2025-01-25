4906c20223c7f11d9d9e16bf1c530eef96e002c9
Abhi787145 <as787145@gmail.com>
2025-01-26 00:17:50 +0530

    Portfolio_0.22

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function showContactInfo() {
-  var contactInfo = document.getElementById("contactInfo");
-  if (contactInfo.style.display === "none" || contactInfo.style.display === "") {
-      contactInfo.style.display = "block";
-  } else {
-      contactInfo.style.display = "none";
-  }
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-link");
+
+  links.forEach(link => {
+    link.addEventListener("click", event => {
+      event.preventDefault();
+
+      // Get the target URL and slide direction
+      const targetUrl = event.target.href;
+      const slideDirection = event.target.dataset.slide || "left";
+
+      // Add sliding transition effect
+      const body = document.body;
+      body.classList.add(`slide-${slideDirection}`);
+
+      setTimeout(() => {
+        window.location.href = targetUrl;
+      }, 500); // Match with the CSS transition duration
+    });
+  });
+});
